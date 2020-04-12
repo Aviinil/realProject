f0f3d83b2a7465205e75ee2f2ca04070fe9502fa
Clement Lavallee <clement.lavallee@hotmail.com>
2020-04-12 18:44:53 +0200

    Bien avancé sur la page d'édition de liste

--- infoListe.js
+++ infoListe.js
@@ -1,71 +1,47 @@
-import React, { useState } from 'react';
-
-
-export function TitreListe(props) {
-    let [showForm, setShowForm] = useState(false);
+export function BarreTache(props) {
+      
     return (
-        <div className="titres">
-            {props.listes.map((liste, index) => {
-                
-                return <div key={index} className="titre-liste"> {liste.titre} 
-                <span className="compteur"> {liste.taches.length}
-                    </span></div>
-            })}
-            {showForm ? <NouvelleListe /> : null}
-            <div className="titre-liste" onClick={() => setShowForm(true)}>
-                <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" >
-                <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="#000" /></svg>
-                <h6 >Nouvelle liste</h6>
-            </div>
-        </div>  
-        
-    )
-
-    
-}
-
-/*export function InfoTache(props) {
-    return (
-        <div className="main__taches">
-            {props.listes.map((liste, index) => {
-                
-                return (
-                <div key={index} className="taches-liste"> 
-                {liste.taches.map((tache, index) =>  <div key={index}><hr/>
-                    <label className="container tache"><span className ="rayable" id={tache.id}>{tache.contenuTache}</span>
-                    <input type="checkbox" className={tache.id} />
-                    <span className="checkmark" onClick={() => Rayer(tache.id)}></span>
-                    <span onClick={() => SupprimerTache(tache.id)}><svg width="24" height="24" className="delete-icon" fill="none" 
-                    xmlns="http://www.w3.org/2000/svg" >
-                    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"  />
-                    </svg></span>
-                    </label> 
-                    
-                </div>)}
-            
+        <div className="sidebar">
+            <fieldset>
+                <div>Titre</div>
+                <input type="text" name="TacheChoisie" defaultValue={props.tache.contenuTache} required></input>
+            </fieldset>
+            <fieldset >
+                <div>Etapes</div>
+                <div className="etapes" >
+                    <div className="tache-etapes">
+                        <input type="checkbox"  />
+                        <input type="text" className="input-etapes" name="etape" defaultValue="En attente" required></input>
+                    </div >
+                    <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.667 7.333v1.334h6.666V7.333H4.667zm3.333-6A6.67 6.67 0 001.333 8 6.67 6.67 0 008 14.667 6.67 6.67 0 0014.667 8 6.669 6.669 0 008 1.333zm0 12A5.34 5.34 0 012.667 8 5.34 5.34 0 018 2.667 5.34 5.34 0 0113.333 8 5.34 5.34 0 018 13.333z" fill="#D63031"/></svg>
                 </div>
-            )})}
-            <hr/>
-        </div>  
-    ) 
-}*/
-
-function NouvelleListe() {
-    // call a l'api pour créer une nouvelle liste puis rediriger vers la page d edition de la liste
-    return (
-        <div className="container__connexion">
-            
-            <form method="post" action="/d"> 
-                <p>
-                
-                    <input type="text" name="nouvelleListe" placeholder="Intitulé de la liste" required></input>
-                </p>
-            </form>
-        
-            
+                <div className="etapes" >
+                    <div className="tache-etapes">  
+                        <input type="checkbox"  />  
+                        <input type="text" className="input-etapes" name="etape" defaultValue="En attente" required></input>
+                    </div>
+                    <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.667 7.333v1.334h6.666V7.333H4.667zm3.333-6A6.67 6.67 0 001.333 8 6.67 6.67 0 008 14.667 6.67 6.67 0 0014.667 8 6.669 6.669 0 008 1.333zm0 12A5.34 5.34 0 012.667 8 5.34 5.34 0 018 2.667 5.34 5.34 0 0113.333 8 5.34 5.34 0 018 13.333z" fill="#D63031"/></svg>
+                </div>
+            </fieldset>
+            <fieldset >
+                <div>Echéance</div>  
+                <div className="calendar">
+                    <input type="date" name="date"  required></input>
+                    <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.333 2h-.666V.667h-1.334V2H4.667V.667H3.333V2h-.666c-.734 0-1.334.6-1.334 1.333V14c0 .733.6 1.333 1.334 1.333h10.666c.734 0 1.334-.6 1.334-1.333V3.333c0-.733-.6-1.333-1.334-1.333zm0 12H2.667V5.333h10.666V14z"/></svg>
+                </div>
+            </fieldset>
+            <fieldset>
+                <div>Note</div>  
+                <textarea type="text" cols="35" rows ="10" name="note"className="tache__text__note" ></textarea>
+            </fieldset>
+            <fieldset className="zone__button">
+                 
+                <input type="submit" className="Button" value="Enregistrer" ></input>
+                <input type="submit" className="Button" value="Annuler" ></input>
+            </fieldset>
         </div>
     )
-}
-
-
-
+}   
--- liste.js
+++ liste.js
@@ -1,13 +1,14 @@
 import { getList, createItem, deleteItem  } from './api.js';
 import React, { useState, useEffect } from 'react';
-import { TitreListe } from './infoListe.js';
-
+import { BarreTache } from './infoListe.js';
+import Modal from 'react-bootstrap/Modal';
 
 
 export function ListeTaches() {
     let [showTaches, setShowTaches] = useState(false);
     let [listes, setListes] = useState([]);
     let [tacheChoisie, setTacheChoisie] = useState();
+    let [detailListe, setDetailListe] = useState();
 
     useEffect(() => {
         async function fetchList() {
@@ -38,13 +39,41 @@ export function ListeTaches() {
             </div>     
                 
             <div className="main">
+                {(detailListe == null) ? 
+                <>
                 <h2>Prochaines tâches</h2>
                 <InfoTache listes={listes} />
+                </>
+                :
+                <>
+                <FocusListe liste={detailListe} /> 
+                </>
+            }
             </div> 
             {showTaches ? <BarreTache tache={tacheChoisie}/> : null}
         </div>
         
     );
+
+    function TitreListe(props) {
+        let [showForm, setShowForm] = useState(false);
+        return (
+            <div className="titres">
+                {props.listes.map((liste, index) => {
+                    
+                    return <div key={index} className="titre-liste" onClick={() =>setDetailListe(liste)}> {liste.titre} 
+                    <span className="compteur"> {liste.taches.length}
+                        </span></div>
+                })}
+                {showForm ? <NouvelleListe /> : null}
+                <div className="titre-liste" onClick={() => setShowForm(true)}>
+                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" >
+                    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="#000" /></svg>
+                    <h6 >Nouvelle liste</h6>
+                </div>
+            </div>   
+        )
+    }
 
     function InfoTache(props) {
         return (
@@ -74,46 +103,101 @@ export function ListeTaches() {
             </div>  
         ) 
     }
-    function BarreTache(props) {
-   
-        
-        return (
-            <div className="sidebar">
-                <fieldset>
-                    <div>Titre</div>
-                    <input type="text" name="TacheChoisie" defaultValue={props.tache.contenuTache} required></input>
-                </fieldset>
-                <fieldset>
-                    <div>Etapes</div>
-                    <input type="text" name="etape" defaultValue="en attente" required></input>
-                    <input type="text" name="etape" defaultValue="en attente" required></input>
-                </fieldset>
-                <fieldset >
-                    <div>Echéance</div>  
-                    <div className="calendar">
-                        <input type="date" name="date"  required></input>
-                        <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M13.333 2h-.666V.667h-1.334V2H4.667V.667H3.333V2h-.666c-.734 0-1.334.6-1.334 1.333V14c0 .733.6 1.333 1.334 1.333h10.666c.734 0 1.334-.6 1.334-1.333V3.333c0-.733-.6-1.333-1.334-1.333zm0 12H2.667V5.333h10.666V14z" fill="#000"/></svg>
-                    </div>
-                </fieldset>
-                <fieldset>
-                    <div>Note</div>  
-                    <textarea type="text" cols="35" rows ="10" name="note"className="tache__text__note" ></textarea>
-                </fieldset>
-                <fieldset className="zone__button">
-                     
-                    <input type="submit" className="Button" value="Enregistrer" ></input>
-                    <input type="submit" className="Button" value="Annuler" ></input>
-                </fieldset>
+
+function FocusListe(props) {
+    // onsubmit input ajout tache et onclick le +, rajouter une tache ds la DB, avec données vides si ce n'est le titre
+    return (
+        <>
+            <div className="header-edition-liste">
+                <h2>{props.liste.titre}</h2>
+                        <div className="suppression-definitive" onClick={() => ModalSuppr(props)}>
+                        <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4h-3.5z" fill="#fff"/>
+                        </svg> Supprimer la liste
+                        </div>
             </div>
-        )
-    }   
+            <div className="taches-liste"> 
+                {props.liste.taches.map((tache, index) =>  
+                    <div key={index} className="taches-liste-unique">
+                        <input type="checkbox" className={tache.id} onChange={() => Rayer(tache.id)} />
+                        <div className="tache-unique" id={tache.id}>
+                        <div className="container-nom-tache"onDoubleClick={showTaches ? () =>setShowTaches(false): () =>setShowTaches(true)} onClick={ () => setTacheChoisie(tache) }>{tache.contenuTache}</div>
+
+                        <div className="delete-div" onClick={() => SupprimerTache(tache.id)}><svg width="24" height="24" className="delete-icon" fill="none" 
+                        xmlns="http://www.w3.org/2000/svg" >
+                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"  />
+                        </svg></div>
+                        
+                        </div>                          
+                    </div>
+                )}
+                
+                    <div id="myModal" class="modal">
+
+                    
+                    <div class="modal-content">
+                        
+                        <h4>Supprimer la liste ?</h4>
+                        <p>Après avoir été supprimée, une liste ne peut pas être récupérée.
+                        Êtes-vous certain(e) de vouloir supprimer la liste "{props.liste.titre}" </p>
+                        <div className="delete-buttons">
+                            <div className="suppression-definitive" onClick={fonctionFetchDelete}>
+                            <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4h-3.5z" fill="#fff"/>
+                            </svg> Supprimer la liste
+                            </div>
+                            <div className="annuler" >
+                            <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" fill="#fff"/>
+                            </svg> Annuler
+                            </div>
+                        </div>
+                    </div>
+
+                    </div>
+                <div className="taches-liste-unique ajout-tache">
+                    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" fill="#000"/></svg>
+                    <div> <input type="text" name="ajouter-tache" placeholder="Ajouter une tâche ..." required></input></div>   
+                </div>
+                <div className="tache-finale"></div>
+            </div>
+        </>
+    )
+
+}
+   
+}
+function fonctionFetchDelete() {
+
+}
+function ModalSuppr(props) {
+  
+    let modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    var close = document.getElementsByClassName("annuler")[0];
+    close.onclick = function() {
+        modal.style.display = "none";
+      }
 }
 
 
-
-
-
+function NouvelleListe() {
+    // call a l'api pour créer une nouvelle liste puis rediriger vers la page d edition de la liste
+    return (
+        <div className="container__connexion">
+            
+            <form method="post" action="/d"> 
+                <p>
+                
+                    <input type="text" name="nouvelleListe" placeholder="Intitulé de la liste" required></input>
+                </p>
+            </form>
+        
+            
+        </div>
+    )
+}
 
 function Rayer(index) {
     // ajouter l'information dans la DB
