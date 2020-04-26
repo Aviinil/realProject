75461f1e4190194586f7a2576d26bb46132b67f9
Jack <56638954+JackThay@users.noreply.github.com>
2020-04-26 22:17:41 +0200

    Ajout table "etapes" et d'un jeu de valeur pour

--- bdd/populate.sql
+++ bdd/populate.sql
@@ -10,7 +10,12 @@ DROP TABLE IF EXISTS etapes CASCADE;
 DROP SEQUENCE IF EXISTS SeqIDutilisateurs;
 DROP SEQUENCE IF EXISTS SeqIDlistes;
 DROP SEQUENCE IF EXISTS SeqIDtaches;
+DROP SEQUENCE IF EXISTS SeqIDetape;
 
+-- >Les IDutilsateurs commence par ressemblent à "1XXXXX" 
+-- >>Les IDlistes commence par ressemblent à "2XXXXX"
+-- >>>Les IDtaches commence par ressemblent à "3XXXXX" 
+-- >>>>Les IDetape commence par ressemblent à "4XXXXX" 
 
 -- création table utilisateurs
 CREATE TABLE utilisateurs 
@@ -24,13 +29,14 @@ CREATE SEQUENCE SeqIDutilisateurs START WITH 10000 INCREMENT BY 10;
 ALTER TABLE utilisateurs ADD CONSTRAINT PK_IDutilisateurs PRIMARY KEY(IDutilisateurs);
 
 -- c'est à partir de là que Oracle SQL et PostgreSQL ne sont pas d'accord
+-- ID: test@gmail.com Pass: password
 INSERT INTO utilisateurs(IDutilisateurs, email, unsecured_password) VALUES (nextval('SeqIDutilisateurs'), 'test@gmail.com', '$2b$10$qQGzUHQlo/Yy7SWVjCaruuR692KRzD1XhuyqXlz6hWejdBl9I7r/a');
 
 -- création table listes
 CREATE TABLE listes 
 (
   IDlistes INTEGER,
-  IDutilisateurs INTEGER REFERENCES utilisateurs (IDutilisateurs),
+  IDutilisateurs INTEGER REFERENCES utilisateurs (IDutilisateurs) NOT NULL,
   titre VARCHAR(20),
   content VARCHAR(50)
 );
@@ -63,7 +69,16 @@ INSERT INTO taches(IDtaches, contenuTache, IDlistes) VALUES (nextval('SeqIDtache
 CREATE TABLE etapes
 (
   IDetape INTEGER,
-  contenuEtape VARCHAR(50)
+  IDtaches INTEGER REFERENCES taches (IDtaches) NOT NULL,
+  contenuEtape VARCHAR(100)
 );
 
--- Je voulais avancer plus loin sur la création de la table etape, mais si le code SQL avant ne marche pas, hein :/
+CREATE SEQUENCE SeqIDetape START WITH 40000 INCREMENT BY 10;
+ALTER TABLE etapes ADD CONSTRAINT PK_IDetape PRIMARY KEY(IDetape);
+
+-- Etape appartenant à la tache "Bosser le projet"
+INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Commencer la base de donnée SQL', 30010);
+INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Tester la BDD sous Oracle SQL', 30010);
+INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Se rendre compte que Oracle SQL et PostgreSQL ont des difference', 30010);
+INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Pester pendant 24h pourquoi tout le monde ne fait pas le même SQL', 30010);
+INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'HAAAAAAAAAAAAAA!!!!!!!!!!!', 30010);
