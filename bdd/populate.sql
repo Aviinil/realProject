df9653008800f3b794737c6d89af783157c71170
Jack <56638954+JackThay@users.noreply.github.com>
2020-04-26 22:39:18 +0200

    Legere amélioration

--- bdd/populate.sql
+++ bdd/populate.sql
@@ -1,84 +1,84 @@
 -- vidage table et sequence
---DROP TABLE utilisateurs CASCADE CONSTRAINT;
-DROP TABLE IF EXISTS utilisateurs CASCADE;
---DROP TABLE listes CASCADE CONSTRAINT;
-DROP TABLE IF EXISTS listes CASCADE;
---DROP TABLE taches CASCADE CONSTRAINT;
-DROP TABLE IF EXISTS taches CASCADE;
+--DROP TABLE utilisateur CASCADE CONSTRAINT;
+DROP TABLE IF EXISTS utilisateur CASCADE;
+--DROP TABLE liste CASCADE CONSTRAINT;
+DROP TABLE IF EXISTS liste CASCADE;
+--DROP TABLE tache CASCADE CONSTRAINT;
+DROP TABLE IF EXISTS tache CASCADE;
 -- Drop table etape
-DROP TABLE IF EXISTS etapes CASCADE;
-DROP SEQUENCE IF EXISTS SeqIDutilisateurs;
-DROP SEQUENCE IF EXISTS SeqIDlistes;
-DROP SEQUENCE IF EXISTS SeqIDtaches;
+DROP TABLE IF EXISTS etape CASCADE;
+DROP SEQUENCE IF EXISTS SeqIDutilisateur;
+DROP SEQUENCE IF EXISTS SeqIDliste;
+DROP SEQUENCE IF EXISTS SeqIDtache;
 DROP SEQUENCE IF EXISTS SeqIDetape;
 
--- >Les IDutilsateurs commence par ressemblent à "1XXXXX" 
--- >>Les IDlistes commence par ressemblent à "2XXXXX"
--- >>>Les IDtaches commence par ressemblent à "3XXXXX" 
+-- METTEZ TOUT AU SINGULIER
+-- >Les IDutilsateur commence par ressemblent à "1XXXXX" 
+-- >>Les IDliste commence par ressemblent à "2XXXXX"
+-- >>>Les IDtache commence par ressemblent à "3XXXXX" 
 -- >>>>Les IDetape commence par ressemblent à "4XXXXX" 
 
--- création table utilisateurs
-CREATE TABLE utilisateurs 
+-- création table utilisateur
+CREATE TABLE utilisateur 
 (
-  IDutilisateurs INTEGER,
+  IDutilisateur INTEGER,
   email VARCHAR(50),
   unsecured_password VARCHAR(60)
 );
 
-CREATE SEQUENCE SeqIDutilisateurs START WITH 10000 INCREMENT BY 10;
-ALTER TABLE utilisateurs ADD CONSTRAINT PK_IDutilisateurs PRIMARY KEY(IDutilisateurs);
+CREATE SEQUENCE SeqIDutilisateur START WITH 10000 INCREMENT BY 10;
+ALTER TABLE utilisateur ADD CONSTRAINT PK_IDutilisateur PRIMARY KEY(IDutilisateur);
 
--- c'est à partir de là que Oracle SQL et PostgreSQL ne sont pas d'accord
 -- ID: test@gmail.com Pass: password
-INSERT INTO utilisateurs(IDutilisateurs, email, unsecured_password) VALUES (nextval('SeqIDutilisateurs'), 'test@gmail.com', '$2b$10$qQGzUHQlo/Yy7SWVjCaruuR692KRzD1XhuyqXlz6hWejdBl9I7r/a');
+INSERT INTO utilisateur(IDutilisateur, email, unsecured_password) VALUES (nextval('SeqIDutilisateur'), 'test@gmail.com', '$2b$10$qQGzUHQlo/Yy7SWVjCaruuR692KRzD1XhuyqXlz6hWejdBl9I7r/a');
 
--- création table listes
-CREATE TABLE listes 
+-- création table liste
+CREATE TABLE liste
 (
-  IDlistes INTEGER,
-  IDutilisateurs INTEGER REFERENCES utilisateurs (IDutilisateurs) NOT NULL,
+  IDliste INTEGER,
+  IDutilisateur INTEGER REFERENCES utilisateur (IDutilisateur) NOT NULL,
   titre VARCHAR(20),
   content VARCHAR(50)
 );
 
-CREATE SEQUENCE SeqIDlistes START WITH 20000 INCREMENT BY 10;
-ALTER TABLE listes ADD CONSTRAINT PK_IDlistes PRIMARY KEY(IDlistes);
+CREATE SEQUENCE SeqIDliste START WITH 20000 INCREMENT BY 10;
+ALTER TABLE liste ADD CONSTRAINT PK_IDliste PRIMARY KEY(IDliste);
 
 -- Ces 2 listes appartiennent à l'utilisateur "test"(numero 10000)
-INSERT INTO listes(IDlistes, titre, content, IDutilisateurs) VALUES (nextval('SeqIDlistes'), 'Coucou', 'something interesting', 10000);
-INSERT INTO listes(IDlistes, titre, content, IDutilisateurs) VALUES (nextval('SeqIDlistes'), 'Yeah', 'something else !', 10000);
+INSERT INTO liste(IDliste, titre, content, IDutilisateur) VALUES (nextval('SeqIDliste'), 'Coucou', 'something interesting', 10000);
+INSERT INTO liste(IDliste, titre, content, IDutilisateur) VALUES (nextval('SeqIDliste'), 'Yeah', 'something else !', 10000);
 
--- création table taches
-CREATE TABLE taches 
+-- création table tache
+CREATE TABLE tache 
 (
-  IDtaches INTEGER,
-  IDlistes INTEGER REFERENCES listes (IDlistes) NOT NULL,
+  IDtache INTEGER,
+  IDliste INTEGER REFERENCES liste (IDliste) NOT NULL,
   contenuTache VARCHAR(50)
   --rajouter un boolean "is_checked" Attention BOOLEAN n'existe que dans PostgreSQL!!
 );
 
-CREATE SEQUENCE SeqIDtaches START WITH 30000 INCREMENT BY 10;
-ALTER TABLE taches ADD CONSTRAINT PK_IDtaches PRIMARY KEY(IDtaches);
+CREATE SEQUENCE SeqIDtache START WITH 30000 INCREMENT BY 10;
+ALTER TABLE tache ADD CONSTRAINT PK_IDtache PRIMARY KEY(IDtache);
 
 -- Tache appartenant à la liste "coucou"
-INSERT INTO taches(IDtaches, contenuTache, IDlistes) VALUES (nextval('SeqIDtaches'), 'Finir le projet', 20000);
+INSERT INTO tache(IDtache, contenuTache, IDliste) VALUES (nextval('SeqIDtache'), 'Finir le projet', 20000);
 -- Tache appartenant à la liste "Yeah"
-INSERT INTO taches(IDtaches, contenuTache, IDlistes) VALUES (nextval('SeqIDtaches'), 'Bosser le projet', 20010);
-INSERT INTO taches(IDtaches, contenuTache, IDlistes) VALUES (nextval('SeqIDtaches'), 'En chier pour le projet', 20010);
+INSERT INTO tache(IDtache, contenuTache, IDliste) VALUES (nextval('SeqIDtache'), 'Bosser le projet', 20010);
+INSERT INTO tache(IDtache, contenuTache, IDliste) VALUES (nextval('SeqIDtache'), 'En chier pour le projet', 20010);
 
-CREATE TABLE etapes
+CREATE TABLE etape
 (
   IDetape INTEGER,
-  IDtaches INTEGER REFERENCES taches (IDtaches) NOT NULL,
+  IDtache INTEGER REFERENCES tache (IDtache) NOT NULL,
   contenuEtape VARCHAR(100)
 );
 
 CREATE SEQUENCE SeqIDetape START WITH 40000 INCREMENT BY 10;
-ALTER TABLE etapes ADD CONSTRAINT PK_IDetape PRIMARY KEY(IDetape);
+ALTER TABLE etape ADD CONSTRAINT PK_IDetape PRIMARY KEY(IDetape);
 
 -- Etape appartenant à la tache "Bosser le projet"
-INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Commencer la base de donnée SQL', 30010);
-INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Tester la BDD sous Oracle SQL', 30010);
-INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Se rendre compte que Oracle SQL et PostgreSQL ont des difference', 30010);
-INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'Pester pendant 24h pourquoi tout le monde ne fait pas le même SQL', 30010);
-INSERT INTO etapes(IDetape, contenuEtape, IDtaches) VALUES (nextval('SeqIDetape'), 'HAAAAAAAAAAAAAA!!!!!!!!!!!', 30010);
+INSERT INTO etape(IDetape, contenuEtape, IDtache) VALUES (nextval('SeqIDetape'), 'Commencer la base de donnée SQL', 30010);
+INSERT INTO etape(IDetape, contenuEtape, IDtache) VALUES (nextval('SeqIDetape'), 'Tester la BDD sous Oracle SQL', 30010);
+INSERT INTO etape(IDetape, contenuEtape, IDtache) VALUES (nextval('SeqIDetape'), 'Se rendre compte que Oracle SQL et PostgreSQL ont des difference', 30010);
+INSERT INTO etape(IDetape, contenuEtape, IDtache) VALUES (nextval('SeqIDetape'), 'Pester pendant 24h pourquoi tout le monde ne fait pas le même SQL', 30010);
+INSERT INTO etape(IDetape, contenuEtape, IDtache) VALUES (nextval('SeqIDetape'), 'HAAAAAAAAAAAAAA!!!!!!!!!!!', 30010);
