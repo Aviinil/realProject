2de1099ea05c4fd055186b06ca534c2200efacca
Jack <56638954+JackThay@users.noreply.github.com>
2020-04-26 23:04:30 +0200

    Adaptation à la nouvelle BDD

--- getSQL/SQL.js
+++ getSQL/SQL.js
@@ -1,46 +1,51 @@
 const utils = require("../bdd/utils");
 
 // Fonction utilisable par l'exterieur
+// <!>Je vais tout mettre au singulier parce que je m'emmele les pinceaux<!>
 module.exports = {
     addListeToUtilisateur,
-    getListesFromUtilisateur,
+    getListeFromUtilisateur,
     deleteListe,
-    addTachesToListe,
-    getTachesFromListe,
-    deleteTache
+    addTacheToListe,
+    getTacheFromListe,
+    deleteTache,
+    addEtapeToListe,
+    getEtapeFromTache,
+    deleteEtape
   };
 
-/* Fonction qui créer une liste pour un utilisateur precis à partir d'un "IDutilisateurs" */
-function addListeToUtilisateur(IDlistes, titre, content, IDutilisateurs, callback) {
-  const query = "INSERT INTO listes(IDlistes, titre, content, IDutilisateurs) VALUES ($1, $2, $3, $4);";
-  utils.executeQuery(query, [IDlistes, titre, content, IDutilisateurs], (err, result) => {
+
+/* Fonction qui créer une liste pour un utilisateur precis à partir d'un "IDutilisateur" */
+function addListeToUtilisateur(IDliste, titre, content, IDutilisateur, callback) {
+  const query = "INSERT INTO liste(IDliste, titre, content, IDutilisateur) VALUES ($1, $2, $3, $4);";
+  utils.executeQuery(query, [IDliste, titre, content, IDutilisateur], (err, result) => {
     if (err) {
       callback(true, err);
     } else if (result.rows.length === 0) {
-      callback(true, `Impossible de retrouver l'utilisateur ${IDutilisateurs}`);
+      callback(true, `Impossible de retrouver l'utilisateur ${IDutilisateur}`);
     } else {
       callback(undefined, result.rows[0]);
     }
   });
 }
-/* Fonction qui recupere les listes d'un utilsateur precis à partir d'un "IDutilisateurs" */
-function getListesFromUtilisateur(IDutilisateurs, callback) {
-  const query = "SELECT * FROM listes WHERE IDutilisateurs=$1";
-  utils.executeQuery(query, [IDutilisateurs], (err, result) => {
+/* Fonction qui recupere les liste d'un utilsateur precis à partir d'un "IDutilisateur" */
+function getListeFromUtilisateur(IDutilisateur, callback) {
+  const query = "SELECT * FROM liste WHERE IDutilisateur=$1";
+  utils.executeQuery(query, [IDutilisateur], (err, result) => {
     if (err) {
       callback(true, err);
     } else if (result.rows.length === 0) {
-      callback(true, `Impossible de retrouver les listes de ${IDutilisateurs}`);
+      callback(true, `Impossible de retrouver les liste de ${IDutilisateur}`);
     } else {
       callback(undefined, result.rows);
      
     }
   });
 }
-/* Fonction qui supprime une listes à partir d'un "IDlistes" */
-function deleteListe(IDlistes, callback) {
-  const query = "DELETE FROM listes WHERE IDlistes=$1";
-  utils.executeQuery(query, [IDlistes], (err, result) => {
+/* Fonction qui supprime une liste à partir d'un "IDliste" */
+function deleteListe(IDliste, callback) {
+  const query = "DELETE FROM liste WHERE IDliste=$1";
+  utils.executeQuery(query, [IDliste], (err, result) => {
     if (err) {
       callback(true, err);
     } else {
@@ -49,10 +54,10 @@ function deleteListe(IDlistes, callback) {
   });
 }
 
-/* Fonction qui ajoute une tache dans une liste precise à partir d'une "IDlistes" */
-function addTachesToListe({ IDtaches, contenuTache, IDlistes }, callback) {
-  const query = "INSERT INTO projects (IDtaches, contenuTache, IDlistes) VALUES ($1, $2, $3)";
-  utils.executeQuery(query, [IDtaches, contenuTache, IDlistes], (err, result) => {
+/* Fonction qui ajoute une tache dans une liste precise à partir d'une "IDliste" */
+function addTacheToListe({ IDtache, contenuTache, IDliste }, callback) {
+  const query = "INSERT INTO tache (IDtache, contenuTache, IDliste) VALUES ($1, $2, $3)";
+  utils.executeQuery(query, [IDtache, contenuTache, IDliste], (err, result) => {
     if (err) {
       callback(true, err);
     } else {
@@ -60,23 +65,23 @@ function addTachesToListe({ IDtaches, contenuTache, IDlistes }, callback) {
     }
   });
 }
-/* Fonction qui recupere les taches d'une liste precise à partir d'une "IDlistes" */
-function getTachesFromListe(IDlistes, callback) {
-    const query = "SELECT * FROM taches WHERE IDlistes=$1";
-    utils.executeQuery(query, [IDlistes], (err, result) => {
+/* Fonction qui recupere les taches d'une liste precise à partir d'une "IDliste" */
+function getTacheFromListe(IDliste, callback) {
+    const query = "SELECT * FROM tache WHERE IDliste=$1";
+    utils.executeQuery(query, [IDliste], (err, result) => {
       if (err) {
         callback(true, err);
       } else if (result.rows.length === 0) {
-        callback(true, `Impossible de retrouver les taches de ${IDlistes}`);
+        callback(true, `Impossible de retrouver les taches de ${IDliste}`);
       } else {
         callback(undefined, result.rows[0]);
       }
     });
 }
-/* Fonction qui supprime une listes à partir d'un "IDlistes" */
-function deleteTache(IDtaches, callback) {
-  const query = "DELETE FROM taches WHERE IDtaches=$1";
-  utils.executeQuery(query, [IDtaches], (err, result) => {
+/* Fonction qui supprime une liste à partir d'un "IDliste" */
+function deleteTache(IDtache, callback) {
+  const query = "DELETE FROM tache WHERE IDtache=$1";
+  utils.executeQuery(query, [IDtache], (err, result) => {
     if (err) {
       callback(true, err);
     } else {
@@ -84,23 +89,34 @@ function deleteTache(IDtaches, callback) {
     }
   });
 }
-/* Fonction qui recupere les etapes d'une tache precise à partir d'une "IDtaches" */
-function getEtapeFromTache(IDlistes, callback) {
-  const query = "SELECT * FROM etapes WHERE IDtaches=$1";
-  utils.executeQuery(query, [IDlistes], (err, result) => {
+/* Fonction qui ajoute une etape dans une tache precise à partir d'une "IDtache" */
+function addEtapeToListe({ IDetape, contenuEtape, IDtache }, callback) {
+  const query = "INSERT INTO etape (IDetape, contenuEtape, IDtache) VALUES ($1, $2, $3)";
+  utils.executeQuery(query, [IDetape, contenuEtape, IDtache], (err, result) => {
+    if (err) {
+      callback(true, err);
+    } else {
+      callback(undefined, result.rows[0]);
+    }
+  });
+}
+/* Fonction qui recupere les etapes d'une tache precise à partir d'une "IDtache" */
+function getEtapeFromTache(IDliste, callback) {
+  const query = "SELECT * FROM etape WHERE IDtache=$1";
+  utils.executeQuery(query, [IDliste], (err, result) => {
     if (err) {
       callback(true, err);
     } else if (result.rows.length === 0) {
-      callback(true, `Impossible de retrouver les taches de ${IDlistes}`);
+      callback(true, `Impossible de retrouver les taches de ${IDliste}`);
     } else {
       callback(undefined, result.rows[0]);
     }
   });
 }
 /* Fonction qui supprime une etape à partir d'un "IDetape" */
-function deleteEtape(IDtaches, callback) {
-const query = "DELETE FROM etapes WHERE IDetapes=$1";
-utils.executeQuery(query, [IDtaches], (err, result) => {
+function deleteEtape(IDtache, callback) {
+const query = "DELETE FROM etape WHERE IDetape=$1";
+utils.executeQuery(query, [IDtache], (err, result) => {
   if (err) {
     callback(true, err);
   } else {
