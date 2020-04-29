2a321f1a47b0dfbc14259f620a193fc70d8ef4d1
Jack <56638954+JackThay@users.noreply.github.com>
2020-04-29 04:48:06 +0200

    Avancé sur le mailer

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,25 +1,21 @@
-const utils = require("../bdd/utils");
 const nodemailer = require('nodemailer');
 
-async function sendEmail({from, toArray, subject, text, html}, callback) {
-    let testAccount = await nodemailer.createTestAccount();
+async function sendEmail(destinataire, callback) {
     let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, 
+        host: "Gmail",
         auth: {
-            user: testAccount.user, 
-            pass: testAccount.pass 
+            user: "todolist.dutas@gmail.com", // Bon on reflechira apres pour proteger les logins
+            pass: "DUTAS2020"  
         }
     });
 
     /*****   todolist.dutas@gmail.com     m2p: DUTAS2020    ***/
     let info = await transporter.sendMail({
         from: 'todolist.dutas@gmail.com ', 
-        to: "", // list of receivers
+        to: destinataire,
         subject: "Confirmation d'inscription", 
-        text: "Bonjour, nous vous confirmons vos inscription", 
-        html: "<b>Bonjour, nous vous confirmons vos inscription</b>"
+        text: "Projet Web Saïd/Clement/Jacky", 
+        html: "<b>Ceci est un test</b>"
     });
     console.log("Message envoyé: %s", info.messageId);
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
@@ -29,21 +25,3 @@ async function sendEmail({from, toArray, subject, text, html}, callback) {
 module.exports = {
     sendEmail
  };
-
-
-
- /*************A COUPER COLLER DANS API.JS ****************/
-
-
- const mailerServices = require("../mailer");
-
- router.get('/email', (req, res, next) => {
-    mailerServices.sendEmail({}, (err, result) => {
-      if (err) {
-        res.status(500).json({ message: err });
-        return;
-      }
-      res.json({ message: 'Message sent: ' + result.response });
-  
-    });
-  });
--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const jwt = require('jsonwebtoken');
 const config = require("./config.js");
 let bodyParser = require('body-parser')
 let cors = require('cors')
+const envoiMail = require("../mailer"); // Pour le mailer de Saïd -- Jack
 const getSQL = require("./getSQL/SQL"); // pour moi quand je créerai des fonctions -- Jack
 let app = express()
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// Pour le mailer, a modifier 
+router.get('/email', (req, res) => {
+  envoiMail.sendEmail(destinataire/* destinataire = une fonction pour mettre l'adresse email ici */, (err, result) => {
+    if (err) {
+      res.status(500).json({ message: err });
+      return;
+    }
+    res.json({ message: 'Message sent: ' + result.response });
+
+  });
+});
 
 app.get('/listes/:id([0-9]*)', (req, res) => {
   getSQL.getListeFromUtilisateur(req.params.id, (err, result) => {
